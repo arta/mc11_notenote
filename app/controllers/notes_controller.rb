e41c977d146c9a231b0eabf123e550bffb68f0d7
Martin Fencl <martin.fencl@gmail.com>
2017-01-20 12:18:51 -0600

    Draw RESTful routes to notes resources, stub controller actions.

--- app/controllers/notes_controller.rb
+++ app/controllers/notes_controller.rb
@@ -1,2 +1,50 @@
 class NotesController < ApplicationController
+  before_action :set_note, only: [:show, :edit, :update, :destroy]
+
+  # GET /notes
+  def index
+
+  end
+
+  # GET /notes/new                     (renders /notes/_form)
+  def new
+
+  end
+
+  # POST /notes
+  #   html:   <form action='/notes', method='post' ..>
+  #   rails:  =form_for @note.. when @note.new_record? == true
+  #   router: post '/notes', to: 'notes#create' ...
+  def create
+
+  end
+
+  # GET /notes/:id
+  def show
+
+  end
+
+  # GET /notes/:id/edit                (renders /notes/_form)
+  def edit
+
+  end
+
+  # PATCH/PUT /notes/:id
+  def update
+
+  end
+
+  # DELETE /notes/:id
+  def destroy
+
+  end
+
+  private
+    def notes_params
+
+    end
+
+    def set_note
+      @note = Note.find params[:id]
+    end
 end
